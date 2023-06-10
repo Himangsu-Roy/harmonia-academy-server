8b800e4f09f003c199583e6ca40e4fc6654c5d77
himangsu roy <himur98@gmail.com>
2023-06-10 14:49:37 +0600

    class add method added

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express = require("express");
-const app = express();
-const jwt = require("jsonwebtoken");
-const morgan = require("morgan");
 const cors = require("cors");
+const app = express();
 require("dotenv").config();
+const morgan = require("morgan");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
 
 // middleware
-app.use(cors())
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
+app.use(morgan("dev"))
 
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ys20m5d.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,12 +34,18 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+    try { 
+        client.connect();
 
+        const addClassCollection = client.db("harmoniaAcademyDb").collection("addClass");
 
         
+        app.post("/addClass", async(req, res) => {
+            const cls = req.body;
+            console.log(cls)
+            const result = await addClassCollection.insertOne(cls);
+            res.send(result);
+        })
 
 
 
@@ -41,7 +54,7 @@ async function run() {
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
