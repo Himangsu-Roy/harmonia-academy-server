0877d9859d31e8d537a66ea56700da520193120e
himangsu roy <himur98@gmail.com>
2023-06-12 23:56:43 +0600

    get payment data

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
-require("dotenv").config();
+const cors = require("cors");
 const morgan = require("morgan");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 
@@ -41,6 +42,8 @@ async function run() {
         const addClassCollection = client.db("harmoniaAcademyDb").collection("addClass");
         const usersCollection = client.db("harmoniaAcademyDb").collection("users");
         const selectCollection = client.db("harmoniaAcademyDb").collection("select");
+        const paymentCollection = client.db("harmoniaAcademyDb").collection("payment");
+        const enrolledCollection = client.db("harmoniaAcademyDb").collection("enrolled");
 
 
         app.post("/addClass", async (req, res) => {
@@ -79,6 +82,7 @@ async function run() {
             res.send(update)
         })
 
+        // feedback add
         app.put("/class/feedback/:id", async (req, res) => {
             const id = req.params.id;
             const feedback = req.body.feedback
@@ -132,12 +136,14 @@ async function run() {
             res.send(result)
         })
 
+        // get all users
         app.get("/users", async (req, res) => {
             const result = await usersCollection.find().toArray()
             res.send(result)
         })
 
 
+        // add a user
         app.post('/user', async (req, res) => {
             const user = req.body;
             const query = { email: user.email }
@@ -160,7 +166,14 @@ async function run() {
         })
 
 
-        //get class by id
+        // get select class
+        app.get("/selected", async (req, res) => {
+            const result = await selectCollection.find().toArray()
+            res.send(result)
+        })
+
+
+        //get select class by id
         app.post('/select', async (req, res) => {
             const { _id } = req.body;
             const existingUser = await selectCollection.findOne({_id});
@@ -172,18 +185,63 @@ async function run() {
                 const result = await selectCollection.insertOne(req.body);
                 res.send(result);
             }
-            console.log(existingUser)
-
-
         });
 
 
-        // get select class
-        app.get("/selected", async (req, res) => {
-            const result = await selectCollection.find().toArray()
+        // select class delete
+        app.delete("/selectClass/:id", async(req, res) => {
+            const id = req.params.id;
+            const result = await selectCollection.deleteOne({_id: id})
             res.send(result)
         })
 
+        // get select class by id
+        app.get("/selectClass/:id", async(req, res) => {
+            const id = req.params.id;
+            const result = await selectCollection.findOne({_id: id})
+            res.send(result)
+        })
+
+
+        // create payment intent
+        app.post('/create-payment-intent', async (req, res) => {
+            const {classPrice} = req.body;
+            console.log(classPrice)
+
+            const amount = parseInt(classPrice * 100);
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
+        }) 
+ 
+
+        // payment related api
+        app.post('/payment', async (req, res) => {
+            const payment = req.body;
+            const {id} = req.body;
+
+            const insertResult = await paymentCollection.insertOne(payment);
+
+            const deleteResult = await selectCollection.deleteOne({_id: id})
+
+            res.send({ insertResult, deleteResult });  
+        })
+
+
+        // get enrolled Data
+        app.get("/enrolled/:email", async(req, res) => {
+            const email = req.params.email;
+            const query = {email: email}
+            console.log(query)
+            const result = await paymentCollection.find(query).toArray();
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
